8bb6062de8b426f0c5c80c681ce4ea86fd53196a
Bognár Pál <bognar.pal@jedlik.eu>
2024-02-23 09:32:54 +0100

    navbar

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-let x = 1;
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
+
+if (btn && nav) {
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('open')
+      nav.classList.toggle('flex')
+      nav.classList.toggle('hidden')
+    });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [".**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+     },
+    },
   },
   plugins: [],
 }

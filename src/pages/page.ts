7cd6bbfc6afe51300eefd8f99dff3cc40ec80512
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2024-04-04 08:52:48 +0200

    Master's Page.ts

--- src/pages/page.ts
+++ src/pages/page.ts
@@ -8,11 +8,16 @@ export class Page {
         this.getHtml(htmlPage).then( (html) => {
             if (this.contentDiv) {
                 this.contentDiv.innerHTML = html;
+                this.getHtmlCallback();
             }
         });
     }
     
-    getHtml(url: string): Promise<string> {
+    getHtmlCallback(){
+        
+    }
+
+    async getHtml(url: string): Promise<string> {
         const requestOptions: RequestInit = {
             method: 'GET',
             redirect: 'follow'
@@ -25,5 +30,56 @@ export class Page {
             .catch( (error) => {
                 throw new Error(error);
             })
+    }
+
+    querySelector<T>(selector: string): T {
+        return document?.querySelector(selector) as T;
+    }
+
+    createElement<T>(htmlTag: string): T {
+        let element = document?.createElement(htmlTag);
+        return element as T;
+    }
+
+    querySelectorAll<T>(selector: string): Array<T> {
+        let l: Array<T> = [];
+        document?.querySelectorAll(selector).forEach(element => {
+            l.push(element as T)
+        });
+        return l;
+    }
+    
+    async fetch<T>(url: string, method: string, body: any = null): Promise<T> {
+        const userInfo = localStorage.getItem('user');   
+        let token = '';
+        if (userInfo) {
+            token = JSON.parse(userInfo).token;
+        }
+        const requestOptions: RequestInit = {
+            method: method,
+            redirect: 'follow',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            },
+            body: body ? JSON.stringify(body) : null
+        };
+
+        return fetch(url, requestOptions)
+            .then( (response) => {
+                if (response.status == 200) {
+                    return response.text();
+                } else if (response.status == 500) {                    
+                    throw response;
+                } else {
+                    throw new Error(`Hiba a back-end hívás során (ErrorCode: ${response.status})`)
+                }
+            })
+            .then( (data) => {
+                if (data) {
+                    return JSON.parse(data) as T
+                }
+                return null as T;
+            })            
     }
 }

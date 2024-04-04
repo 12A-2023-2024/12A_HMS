f46554f269f4092f4fd4f70bf7cfa674d5ad7c2c
Szaloki Peter <szaloki.peter.krisztian@students.jedlik.eu>
2024-04-04 09:25:33 +0200

    Login for every other querry

--- src/pages/wellness/welness.ts
+++ src/pages/wellness/welness.ts
@@ -6,5 +6,27 @@ export class WellnessPage extends Page {
         super('/src/pages/wellness/wellness.html')
     }
 
+    login(){
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        const raw = JSON.stringify({
+        "loginName": "admin",
+        "password": "admin"
+        });
+
+        const requestOptions : RequestInit = {
+          method: "POST",
+          headers: myHeaders,
+          body: raw,
+          redirect: "follow"
+        };
+
+        fetch("https://hms.jedlik.cloud/api/login", requestOptions)
+          .then((response) => response.text())
+          .then((result) => console.log(result))
+          .catch((error) => console.error(error));
+    }
+    
+
     
 }

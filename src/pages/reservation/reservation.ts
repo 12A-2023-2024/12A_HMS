72895aaf0b2b701ef9ccc7d2b4c66226b4266a52
abos.gergo <gergo.abos@gmail.com>
2024-04-05 07:47:43 +0100

    Added page

---
+++ src/pages/reservation/reservation.ts
@@ -0,0 +1,8 @@
+import { Page } from "../page.js";
+
+export class ReservationPage extends Page {
+
+    constructor() {
+        super('/src/pages/reservation/reservation.html')
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { ContactPage } from "./pages/contact/contact.js";
 import { HomePage } from "./pages/home/home.js";
 import { IntroductionPage } from "./pages/introduction/introduction.js";
+import { ReservationPage } from "./pages/reservation/reservation.js";
 
-export const routes: {[key: string]: {page: any}} = {
+export const routes: { [key: string]: { page: any } } = {
     '': {
         page: HomePage
     },
@@ -11,6 +12,9 @@ export const routes: {[key: string]: {page: any}} = {
     },
     'contact': {
         page: ContactPage
+    },
+    'reservation': {
+        page: ReservationPage
     }
 
 }

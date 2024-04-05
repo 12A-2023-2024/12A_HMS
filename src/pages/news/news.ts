1a30e6a1a7f419a1dd04fd40d17cec3517557361
Préts Barnabás <prets.barnabas@gmail.com>
2024-04-05 08:36:53 +0200

    super :(

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,8 +1,20 @@
 import { Page } from "../page.js";
+import { Hir } from "./hir.js";
 
 export class NewsPage extends Page {
 
     constructor() {
         super('/src/pages/news/news.html')
+        this.generateNews()
+    }
+
+    generateNews(){
+        let title = "haha";
+        let text = "lorem";
+        let source = "";
+        let alt = "alt";
+
+        new Hir(title,text,source,alt);
+        
     }
 }

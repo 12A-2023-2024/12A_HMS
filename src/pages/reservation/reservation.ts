a8f90657da60260efcf990b58efc0a22e25b201e
abos.gergo <gergo.abos@gmail.com>
2024-04-11 07:08:31 +0100

    Filtering bar started

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -3,6 +3,50 @@ import { Page } from "../page.js";
 export class ReservationPage extends Page {
 
     constructor() {
-        super('/src/pages/reservation/reservation.html')
+        super('/src/pages/reservation/reservation.html');
     }
+
+    override getHtmlCallback() {
+        const startDateField = document.getElementById('startDate');
+        startDateField?.setAttribute('min', todayString());
+        startDateField?.setAttribute('value', tomorrowString())
+        const endDateField = document.getElementById('endDate');
+        const startDate = startDateField?.getAttribute('value');
+        addOneDayToString('2005-12-11')
+    }
+}
+
+
+function dateToString(date: Date): string {
+    const dd = date.getDate();
+    const mm = date.getMonth() + 1; //January is 0!
+    const yyyy = date.getFullYear();
+
+    let dateString = yyyy.toString();
+
+    dateString += '-';
+    if (mm < 10) {
+        dateString += '0' + mm;
+    }
+    dateString += '-';
+    if (dd < 10) {
+        dateString += '0' + dd;
+    }
+
+    return dateString
+}
+function todayString(): string {
+    const date = new Date()
+    return dateToString(date)
+}
+function tomorrowString(): string {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    return dateToString(tomorrow)
+}
+function addOneDayToString(date: string): string {
+    const day = parseInt(date.slice(8))
+    const newDay = (day + 1) % 31
+    console.log(day)
+    return date
 }

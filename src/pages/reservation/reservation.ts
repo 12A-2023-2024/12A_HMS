4dc492077198f352fca9370aed216c078c37fc6f
abos.gergo <gergo.abos@gmail.com>
2024-04-11 07:37:09 +0100

    Finished starting config

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -11,10 +11,10 @@ export class ReservationPage extends Page {
         startDateField?.setAttribute('min', todayString());
         startDateField?.setAttribute('value', tomorrowString());
         const endDateField = document.getElementById('endDate');
-        const startDate = startDateField?.getAttribute('value');
+        endDateField?.setAttribute('value', addOneDayToString(tomorrowString()));
+        endDateField?.setAttribute('min', addOneDayToString(tomorrowString()));
     }
 }
-
 
 function dateToString(date: Date): string {
     const dd = date.getDate();
@@ -33,20 +33,22 @@ function dateToString(date: Date): string {
         dateString += '0';
     }
     dateString += dd;
-    console.log(dateString)
     return dateString
 }
+
 function todayString(): string {
     const date = new Date()
     return dateToString(date)
 }
+
 function tomorrowString(): string {
     const tomorrow = new Date();
     tomorrow.setDate(tomorrow.getDate() + 1);
     return dateToString(tomorrow)
 }
+
 function addOneDayToString(date: string): string {
     const day = parseInt(date.slice(8))
     const newDay = (day + 1) % 31
-    return date
+    return date.slice(0,8) + newDay
 }

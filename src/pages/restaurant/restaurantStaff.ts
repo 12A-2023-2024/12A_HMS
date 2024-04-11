bc3c6d153ae4f85388e12f8d517b776456e2fe9f
[Máté Varga] <[varga.mate.gellert@students.jedlik.eu]>
2024-04-11 09:37:41 +0200

    +/- buttons work, started dinamic load #6

--- src/pages/restaurant/restaurantStaff.ts
+++ src/pages/restaurant/restaurantStaff.ts
@@ -6,22 +6,64 @@ export class RestaurantStaffPage extends Page {
     this.addEventListeners();
   }
 
+  loadMeals(): void {      
+    // copied form library
+    // rewrite
+        
+      const userInfo = localStorage.getItem('user');   
+      let token = '';
+      if (userInfo) {
+          token = JSON.parse(userInfo).token;
+      }
+      const requestOptions: RequestInit = {
+          method: method,
+          redirect: 'follow',
+          headers: {
+              "Content-Type": "application/json",
+              "Authorization": token
+          },
+          body: body ? JSON.stringify(body) : null
+      };
+
+      return fetch(url, requestOptions)
+          .then( (response) => {
+              if (response.status == 200) {
+                  return response.text();
+              } else if (response.status == 500) {                    
+                  throw response;
+              } else {
+                  throw new Error(`Hiba a back-end hívás során (ErrorCode: ${response.status})`)
+              }
+          })
+          .then( (data) => {
+              if (data) {
+                  return JSON.parse(data) as T
+              }
+              return null as T;
+          })            
+  }
+
+  displayError(error: any, selector: string): void {
+      if (error instanceof Response) {
+          error.json().then( (msg) => {
+              this.querySelector<HTMLElement>(selector).innerText = msg.message;
+          })
+      }
+      else {
+          this.querySelector<HTMLElement>(selector).innerText = error;
+      }
+  }
+
   addEventListeners(): void {
-    const decrementButtons = document.querySelectorAll('.decrementButton');
-    const incrementButtons = document.querySelectorAll('.incrementButton');
-
-    decrementButtons.forEach(button => {
-      button.addEventListener('click', (event) => {
-        const inputField = (event.target as Element).nextElementSibling as HTMLInputElement;
+    document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.matches('.decrementButton')) {
+        const inputField = target.parentElement?.querySelector('input') as HTMLInputElement;
         this.removeMeal(inputField);
-      });
-    });
-
-    incrementButtons.forEach(button => {
-      button.addEventListener('click', (event) => {
-        const inputField = (event.target as Element).previousElementSibling as HTMLInputElement;
+      } else if (target.matches('.incrementButton')) {
+        const inputField = target.parentElement?.querySelector('input') as HTMLInputElement;
         this.addMeal(inputField);
-      });
+      }
     });
   }
 
@@ -36,6 +78,6 @@ export class RestaurantStaffPage extends Page {
     if (mealCount > 0) {
         mealCount--;
     }
-    inputField.value = mealCount.toString();
+    inputField.value = mealCount.toString();  
   }
 }

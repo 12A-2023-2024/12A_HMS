ab69ce59f74280d7dde29e6bb4fe268dbc8563a6
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2024-04-11 08:52:44 +0200

    COcktail card done

--- src/components/cocktailCard/cocktailCard.ts
+++ src/components/cocktailCard/cocktailCard.ts
@@ -7,17 +7,38 @@ export class CocktailCard{
     category: string = "";
     img: string = "";
 
-    constructor(name: string, abv: number, description: string, category: string) {
+    constructor(name: string, abv: number, description: string, category: string, img: string,  place: string) {
         
         this.name = name;
         this.abv = abv;
         this.description = description;
         this.category = category;
+        this.img = img;
         
-        const html = this.getHtml()
-        const wrapperDiv = document.createElement("div")
-        wrapperDiv.innerHTML = html
-        this.html = wrapperDiv
+        this.getHtml("src/components/cocktailCard/cocktailCard.html").then((html)=>{
+            const wrapperDiv = document.createElement("div")
+            wrapperDiv.classList.add("shadow-secondary-5")
+            wrapperDiv.innerHTML = html
+            const imgH = wrapperDiv.querySelector(".ctCardImg") as HTMLImageElement
+            const nameH = wrapperDiv.querySelector(".ctName") as HTMLElement
+            const descriptionH = wrapperDiv.querySelector(".ctDescription") as HTMLElement
+            const abvH = wrapperDiv.querySelector(".ctABV") as HTMLElement
+            console.log(descriptionH)
+            if(imgH && nameH && descriptionH && abvH){
+                nameH.innerText = name;
+                descriptionH.innerText = description;
+                abvH.innerText = abv.toString() + " ·";
+                imgH.src = img;
+
+
+            }
+            this.html = wrapperDiv.firstElementChild as HTMLElement
+            const wrap = document.querySelector(place)
+            
+
+            wrap?.appendChild(this.html)
+        })
+        
             
             
 
@@ -25,13 +46,30 @@ export class CocktailCard{
         
     }
     
-    getHtml() {
-        return `
-        <img class="ctCardImg" src="" alt="">
-<div class="ctContent">
-    <p class="ctName">${this.name}</p>
-    <p><span class="ctABV">${this.abv}</span> <span class="CTdescription">${this.description}</span></p>
-</div>`
+    // getHtml() {
+    //     return `
+    //     <div class="ctCard">
+    //     <img class="ctCardImg" src="https://fastly.picsum.photos/id/988/200/300.jpg?hmac=t-oW7bwXaruDMMMz6vIk1GO5lfOolflGxHfJfheVvc8" alt="">
+    //     <div class="ctContent">
+    //         <p class="ctName text-base text-center">${this.name}</p>
+    //         <p class="text-xs ctDescription"><span class="ctABV font-bold">${this.abv}·</span> <span class="CTdescription">${this.description}</span></p>
+    //     </div>
+    //     <div class="ctBackgroundBox"></div>
+    // </div>`
+    // }
+    getHtml(url: string): Promise<string> {
+        const requestOptions: RequestInit = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        return fetch(url, requestOptions)
+            .then( (response) => {
+                return response.text()
+            })
+            .catch( (error) => {
+                throw new Error(error);
+            })
     }
     
 }
--- src/pages/cocktailbar/cocktailBar.ts
+++ src/pages/cocktailbar/cocktailBar.ts
@@ -5,13 +5,8 @@ export class CocktailBar extends Page {
 
     constructor() {
         super('/src/pages/cocktailBar/cocktailbar.html')
-        const cocktailWrap = document.querySelector(".cocktailWrapper")
-        const cocktailCard = new CocktailCard("Mojito", 10, "A refreshing cocktail", "Longdrink")
-        if(cocktailWrap)
-        {
-            cocktailWrap.appendChild(cocktailCard.html!)
-            
-            
-        }
+        const cocktailWrap = document.querySelector("body")
+        const cocktailCard = new CocktailCard("Fiinom valami", 60, "THE only cocktail", "Longdrink", "https://baristacoffee.ca/img/Recette-Expresso-Martini.png", ".cocktailWrapper" )
+        
     }
 }

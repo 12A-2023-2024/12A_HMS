36d479ff89f88f0f767031d6886dc6fdd0194533
[Máté Varga] <[varga.mate.gellert@students.jedlik.eu]>
2024-04-18 21:39:25 +0200

    loading fixed #6

--- src/pages/restaurant/restaurantStaff.ts
+++ src/pages/restaurant/restaurantStaff.ts
@@ -7,72 +7,66 @@ export class RestaurantStaffPage extends Page {
   meals:IMeal[]=[];
   constructor() {
     super("/src/pages/restaurant/restaurantStaff.html");
-    //this.loadCategories();
+    this.loadCategories();
     this.loadMeals();
     this.addEventListeners();
   }
 
-  // loadCategories(): void {
-  //   this.fetch<ICategory[]>('https://hms.jedlik.cloud/api/restaurant/categories','GET')
-  //   .then((arr:ICategory[])=>{
-  //     this.categories= arr;
-  //     this.querySelector<HTMLElement>('.grid').innerHTML = this.categories.map(c => {
-  //       return `<div class="category relative block text-left justify-center">
-  //         <div class="category inline-flex justify-center w-full rounded-md border border-gray-300 
-  //         shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2
-  //         focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 text-xl">
-  //         ${c.name}
-  //         </div>
-  //       </div>`;
-  //     }).join('');
-  //   });
-  // }  
-
-  //working on joining the 2 functions
-  loadMeals(): void {
+  loadCategories(): void {
     this.fetch<ICategory[]>('https://hms.jedlik.cloud/api/restaurant/categories','GET')
     .then((arr:ICategory[])=>{
       this.categories= arr;
-      this.fetch<IMeal[]>('https://hms.jedlik.cloud/api/restaurant/menuitems','GET')
-    .then((arr:IMeal[])=>{
-      this.meals= arr;
-      this.querySelector<HTMLElement>('.grid').innerHTML = this.categories.map(m => {
-        this.meals.map(c => {
-          return `
-        <div class="category relative block text-left justify-center">
-        <div id="mainCoursesTitle" class="category inline-flex justify-center w-full rounded-md border border-gray-300 
-        shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2
-        focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 text-xl">
-        ${c.name}
-        </div>
+      this.querySelector<HTMLElement>('.grid').innerHTML += this.categories.map(c => {
+        return `<div class="category relative block text-left justify-center" data-category-name="${c.name}">
+          <div class="inline-flex justify-center w-full rounded-md border border-gray-300 
+          shadow-sm px-4 py-2 bg-white font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2
+          focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 text-xl">
+          ${c.name}
+          </div>
+        </div>`;
+      }).join('');
+    });
+  }  
 
-        <div class="origin-top-right right-auto mt-2 w-full rounded-md shadow-lg bg-white ring-1 
-    ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="category">
-            <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
-                <div class="flex justify-between items-center">
-                    <p class="text-xl font-bold">${m.name}</p>
+  loadMeals(): void {
+    this.fetch<IMeal[]>('https://hms.jedlik.cloud/api/restaurant/menuitems','GET')
+    .then((arr:IMeal[])=>{
+      this.meals = arr;
+  
+      const categories = document.querySelectorAll('.category');
+      categories.forEach(c => {
+        const categoryName = c.getAttribute('data-category-name');
+        this.meals.forEach(meal => {
+          if (meal.categoryName == categoryName) {
+            c.innerHTML += `
+              <div class="origin-top-right right-auto mt-2 w-full rounded-md shadow-lg bg-white ring-1 
+              ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="category">
+                <div class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                  <div class="flex justify-between items-center">
+                    <p class="text-xl font-bold">${meal.name}</p>
                     <div class="flex items-center">
-                        <button class="decrementButton border-2 border-black w-8 h-8 bg-slate-200 font-bold
-                         text-xl text-center m-1">
-                        -</button> 
-                        <input type="text" disabled value="0" 
-                        class="mealCount box-border w-8 h-8 border-2 border-black text-center m-1">
-                        <button class="incrementButton border-2 border-black w-8 h-8 bg-slate-200 font-bold
-                         text-xl text-center m-1">
-                        +</button>
+                      <button class="decrementButton border-2 border-black w-8 h-8 bg-slate-200 font-bold
+                      text-xl text-center m-1">
+                      -</button> 
+                      <input type="text" disabled value="0" 
+                      class="mealCount box-border w-8 h-8 border-2 border-black text-center m-1">
+                      <button class="incrementButton border-2 border-black w-8 h-8 bg-slate-200 font-bold
+                      text-xl text-center m-1">
+                      +</button>
                     </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-    </div>`;
-        });}).join('');
+              </div>
+            </div>`;
+          }
+        });
       });
-  });
+    });
   }
 
   fetch<T>(url: string, method: string, body: any = null): Promise<T> {
     const userInfo = localStorage.getItem('user');   
-    let token = 'M3EJVSK7OLKLZW52YU4IPN3RO5X4P4KSE9AY1ZE93PI4Y3HPIOZL6XGQVPSL5FU12VNWR4E5A16VGWHZXF0TYLVK5CJESAINLFRA2CCNIJVSUWRV6AX9WSRM';
+    let token = 'FLCSPJG8VKOQE7JCRDCZO3NL9HVO541C30MNA2IR71S076FDTPLPJR9UZC05F4EB9ISSQV2N6SHTFH2GYC5E90ZT8NS8DW5HUSBMODEIDJ51RLV77U85HT36';
     if (userInfo) {
         token = JSON.parse(userInfo).token;
     }

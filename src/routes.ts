4e14b886a3f54e94ef645659332a34dfabf904ec
VerbaViktorJedlik <verba.viktor@students.jedlik.eu>
2024-04-18 09:36:25 +0200

    routes work

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { ContactPage } from "./pages/contact/contact.js";
 import { HomePage } from "./pages/home/home.js";
 import { IntroductionPage } from "./pages/introduction/introduction.js";
+import { RoomtypeAdminPage } from "./pages/rooms/roomtype_admin.js";
+import { RoomsAdminPage } from "./pages/rooms/rooms_admin.js";
 
 export const routes: { [key: string]: { page: any } } = {
     '': {
@@ -11,5 +13,11 @@ export const routes: { [key: string]: { page: any } } = {
     },
     'contact': {
         page: ContactPage
+    },
+    'roomtype_admin': {
+        page: RoomtypeAdminPage
+    },
+    'rooms_admin': {
+        page: RoomsAdminPage
     }
 }

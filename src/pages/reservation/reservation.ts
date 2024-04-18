ed47e8fb27e0149059df7f6090512c1960d795e2
abos.gergo <gergo.abos@gmail.com>
2024-04-18 07:42:30 +0100

    input bar finished

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -1,4 +1,5 @@
 import { Page } from "../page.js";
+import { Datest } from "./date.js";
 
 export class ReservationPage extends Page {
 
@@ -7,48 +8,42 @@ export class ReservationPage extends Page {
     }
 
     override getHtmlCallback() {
+
+        //Set default values for date picker
         const startDateField = document.getElementById('startDate');
-        startDateField?.setAttribute('min', todayString());
-        startDateField?.setAttribute('value', tomorrowString());
+        startDateField?.setAttribute('min', new Datest().toString());
+        startDateField?.setAttribute('value', new Datest().addDays(1).toString());
+
         const endDateField = document.getElementById('endDate');
-        endDateField?.setAttribute('value', addOneDayToString(tomorrowString()));
-        endDateField?.setAttribute('min', addOneDayToString(tomorrowString()));
+        endDateField?.setAttribute('value', new Datest().addDays(2).toString());
+        endDateField?.setAttribute('min', new Datest().addDays(2).toString());
+
+        addCallBacks();
     }
 }
 
-function dateToString(date: Date): string {
-    const dd = date.getDate();
-    const mm = date.getMonth() + 1; //January is 0!
-    const yyyy = date.getFullYear();
 
-    let dateString = yyyy.toString();
+function addCallBacks() {
 
-    dateString += '-';
-    if (mm < 10) {
-        dateString += '0';
-    }
-    dateString += mm;
-    dateString += '-';
-    if (dd < 10) {
-        dateString += '0';
-    }
-    dateString += dd;
-    return dateString
-}
+    //Align endDate min and value to startDate (endDate is the day after startDate)
+    const startDateField = document.getElementById('startDate');
+    const endDateField = document.getElementById('endDate');
+    startDateField?.addEventListener('change', (e) => {
+        let nextDay = new Datest().fromString(e?.target?.value).addDays(1).toString()
+        endDateField?.setAttribute('value', nextDay);
+        endDateField?.setAttribute('min', nextDay);
+    })
 
-function todayString(): string {
-    const date = new Date()
-    return dateToString(date)
-}
-
-function tomorrowString(): string {
-    const tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    return dateToString(tomorrow)
-}
-
-function addOneDayToString(date: string): string {
-    const day = parseInt(date.slice(8))
-    const newDay = (day + 1) % 31
-    return date.slice(0,8) + newDay
+    //Add functionality to addPersone and removePerson buttons
+    const personCountField = document.getElementById('personCount') as HTMLElement;
+    const addPersonButton = document.getElementById('addPerson');
+    addPersonButton?.addEventListener('click', () => {
+        personCountField.setAttribute('value', (Number(personCountField.getAttribute('value')) + 1).toString());
+    });
+    const removePersonButton = document.getElementById('removePerson');
+    removePersonButton?.addEventListener('click', () => {
+        const value = Number(personCountField.getAttribute('value'));
+        const newValue = Math.max(1, value - 1);
+        personCountField.setAttribute('value', newValue.toString());
+    });
 }

2dd672a75a0467dd02c7ecf35bb772ae6d2e79bd
csiguszka <szabo.taddeus@students.jedlik.eu>
2024-04-18 09:02:14 +0200

    introduction slider done

--- src/pages/introduction/introduction.ts
+++ src/pages/introduction/introduction.ts
@@ -1,9 +1,11 @@
 import { Page } from "../page.js";
+import { introductionModel } from "./introductionModel.js";
 import { Login } from "./login.js";
 import { Scroll } from "./scroll.js";
 
 export class IntroductionPage extends Page {
 
+    data: introductionModel[] |undefined
     constructor() {
         super('/src/pages/introduction/introduction.html');
     }
@@ -11,18 +13,37 @@ export class IntroductionPage extends Page {
     override getHtmlCallback(){
         this.login()
         this.getScrollImgs()
-        this.createScroll()
     }
 
     createScroll(){
         const container = document.querySelector<HTMLElement>('.scroll-container');
+        if (this.data && container){
+            this.data.forEach(model => {
+                if (model.section === "Fejrész"){
+                    container.innerHTML += `<div
+                    class="snap-center w-full flex-shrink-0 flex items-center justify-center text-8xl">
+                    <img class="object-cover w-full max-h-80" src="${model.pictureUrl}" alt="${model.alt}">
+                    </div>`
+                }
+            });
+        }else{
+            console.log("Hülye vagy 2x-esen")
+        }
+
         if (container){
-            new Scroll(container, 1000)
+            new Scroll(container, 2000)
         }
     }
 
     getScrollImgs(){
-        
+        const url: string = "https://hms.jedlik.cloud/api/about/introduction";
+        const method: string = "GET";
+        this.fetch<introductionModel[]>(url, method).then(
+            result => {
+                this.data = result;
+                this.createScroll();
+            }
+        );
     }
 
     login(){
@@ -36,6 +57,6 @@ export class IntroductionPage extends Page {
         console.log(data)
         data.then( (result) => {
             localStorage.setItem('user', JSON.stringify(result));
-        })        
+        })
     }
 }

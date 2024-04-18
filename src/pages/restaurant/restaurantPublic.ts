7e403a6e07a42b3341572846743263846c086451
Tiszolczi Jacint <tiszolczi.jacint@students.jedlik.eu>
2024-04-18 09:04:23 +0200

    added MenuItem class

--- src/pages/restaurant/restaurantPublic.ts
+++ src/pages/restaurant/restaurantPublic.ts
@@ -6,3 +6,20 @@ export class RestaurantPublicPage extends Page {
   }
     
 }
+class MenuItem {
+    public id: number;
+    public name: string;
+    public price: number;
+    public description: string;
+    public categoryId: number;
+    public images: [];
+
+    constructor(id: number, name: string, price: number, description: string, categoryId: number, images: []) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.categoryId = categoryId;
+        this.images = images;
+    }
+}

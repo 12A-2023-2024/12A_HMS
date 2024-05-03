5ac37cb0fdf9c055fe4b1e75d67a56436c4da160
Préts Barnabás <prets.barnabas@gmail.com>
2024-05-03 08:51:34 +0200

    post jo :)

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,5 +1,6 @@
 import { Page } from "../page.js";
 import { Hir } from "./hir.js";
+import { Login } from "../introduction/login.js";
 
 export class NewsPage extends Page {
 
@@ -22,6 +23,7 @@ export class NewsPage extends Page {
                 let reverse = false
                 for (let i = 0; i < news.length; i++) {
                     if(i%2==1) reverse = true
+                    else reverse = false
                     let article: Hir = news[i] as Hir
                     article.date = article.date.slice(0,10)
                     this.generateArticle(reverse,article)
@@ -70,41 +72,52 @@ export class NewsPage extends Page {
     async postNews(){
       var filename = this.getImgName(this.querySelector<HTMLInputElement>("#imgUpload"));
       var file = await this.getBase64(this.querySelector<HTMLInputElement>("#imgUpload"));
-      console.log(file)
       var date = this.getCurrentDate();
-      var text = this.querySelector<HTMLElement>("#uploadMainText").textContent;
-      var title = this.querySelector<HTMLElement>("#uploadHeaderText").textContent;
+      var text = this.querySelector<HTMLTextAreaElement>("#uploadMainText").value;
+      var title = this.querySelector<HTMLTextAreaElement>("#uploadHeaderText").value;
 
       var canProceed = true;
       if(!filename){
+        console.log("filename")
         canProceed = false
       }
       if(!file){
+        console.log("file")
+
         canProceed = false
       }
       if(!date){
+        console.log("date")
+
         canProceed = false
       }
       if(!text){
+        console.log("text")
+
         canProceed = false
       }
       if(!title){
+        console.log("title")
+
         canProceed = false;
       }
 
-      if(!canProceed) return false
-
-      const body = {
-        date: date,
-        text: text,
-        title: title,
-        image: {
-            filename: filename,
-            file: file
-        },
+      if(canProceed){
+        const body = {
+          date: date,
+          text: text,
+          title: title,
+          image: {
+              filename: filename,
+              file: file
+          },
+        }
+  
+        await this.login()
+  
+        this.fetch<null>("https://hms.jedlik.cloud/api/about/news", "POST", body)
       }
 
-      this.fetch<null>("https://hms.jedlik.cloud/api/about/news", "POST", body)
 
     }
 
@@ -129,6 +142,19 @@ export class NewsPage extends Page {
       var file = filelist[0];
       if(file) return file.name
     }
+
+    async login(){
+      const url: string = "https://hms.jedlik.cloud/api/login"
+      const method: string = "POST"
+      const body: any= {
+          "loginName": "admin",
+          "password": "admin"
+       }
+      const data = this.fetch<Login>(url, method, body)
+      await data.then( (result) => {
+          localStorage.setItem('user', JSON.stringify(result));
+      })        
+  }
 
 
     parseResult(result:string){

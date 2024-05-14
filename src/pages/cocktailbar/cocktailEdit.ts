772fb9fa70034019675f99d405133b7aace5b6ab
Zoltai Balázs <zoltai.balazs@students.jedlik.eu>
2024-05-14 09:38:28 +0200

    I don't even know

--- src/pages/cocktailbar/cocktailEdit.ts
+++ src/pages/cocktailbar/cocktailEdit.ts
@@ -3,6 +3,23 @@ import { Page } from "../page.js";
 export class CocktailEdit extends Page {
 
     constructor() {
-        super('/src/pages/cocktailbar/cocktailEdit.html')
+        super('/src/pages/cocktailbar/cocktailEdit.html');
+        this.addEventListeners();
+    }
+
+    addEventListeners() {
+        this.contentDiv?.querySelector('button#btnShowAddNew')?.addEventListener('click', () => {            
+            document.querySelector('div#EditModal')?.classList.remove('hidden');
+            document.querySelector('div#EditModal')?.classList.add('grid');
+        });
+
+        this.contentDiv?.querySelector('input#btnCancel')?.addEventListener('click', () => {
+            document.querySelector('div#EditModal')?.classList.remove('grid');
+            document.querySelector('div#EditModal')?.classList.add('hidden');
+        });
+
+        this.contentDiv?.querySelector('input#btnAddNew')?.addEventListener('click', () => {
+            const name = this.contentDiv?.querySelector('#')
+        });
     }
 }

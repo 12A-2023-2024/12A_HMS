032c5d44c5bfffa9b32b40db1ee966124d23f4dc
[Zoltai Balazs] <balazs.zoltai@gmail.com>
2024-05-19 11:39:39 +0200

    new header layout with del button

--- src/pages/cocktailbar/cocktailEdit.ts
+++ src/pages/cocktailbar/cocktailEdit.ts
@@ -4,66 +4,39 @@ import { CocktailNav } from "./components/cocktailNav/cocktailNav.js";
 import { ctQueries } from "./queries.js";
 
 export class CocktailEdit extends Page {
-    
+
     query: ctQueries | null = null;
-    cocktailCards: CocktailCard[] | null = null;
     navItems: CocktailNav[] = [];
+    cocktailCards: CocktailCard[] | null = null;
 
     constructor() {
         super('/src/pages/cocktailbar/cocktailEdit.html');
         
-        
         this.query = new ctQueries();
+        this.cocktailCards = [];
         
-        this.query?.getCategories().then((result)=>{
-            document.getElementsByClassName("cocktailWrapper")[0].innerHTML = "";
-            
+        this.query.getCategories().then((result)=>{
+      
             result.forEach(({id, name})=>{
-                this.navItems?.push(new CocktailNav(id, name, ".ctLocalNavigation", this));                  
+                this.navItems.push(new CocktailNav(id, name, ".ctLocalNavigation", this))
+                console.log(name);
             })
             console.log(this.navItems);
-
         })
-        this.changeToCategory("Koktélok");
-    }
-
-    addEventListeners() {
-        const editModal = document.querySelector('div#EditModal');        
-
-        this.contentDiv?.querySelector('button#btnShowAddNew')?.addEventListener('click', () => {            
-            editModal?.classList.remove('hidden');
-            editModal?.classList.add('grid');
-        });
-
-        this.contentDiv?.querySelector('button#btnCancel')?.addEventListener('click', () => {
-            editModal?.classList.remove('grid');
-            editModal?.classList.add('hidden');
-        });
-
-        this.contentDiv?.querySelector('button#btnConfirm')?.addEventListener('click', () => {
-            const name = this.contentDiv?.querySelector('#nameInput')?.textContent;            
-            const abv = this.contentDiv?.querySelector('#abvInput')?.textContent;
-            const desc = this.contentDiv?.querySelector('#descriptionInput')?.textContent;
-            const category = this.contentDiv?.querySelector('#descriptionInput') as HTMLSelectElement;
-            const selectedCategory = category?.options[category.selectedIndex].text;
-            
-
-        });
     }
 
     changeToCategory(category:string)
     {
         this.cocktailCards = []
         this.query?.getCocktails().then((result)=>{
-            this.navItems?.forEach((n)=>{
-                document.getElementsByClassName("cocktailWrapper")[0].innerHTML = "";
-
+            document.getElementsByClassName("cocktailWrapper")[0].innerHTML = "";
+            this.navItems.forEach((n)=>{
                 if(n.html?.classList.contains("selectedNav"))
                 {
                     n.html.classList.remove("selectedNav")
                 }
             })
-            let name = this.navItems?.find((n) => {
+            let name = this.navItems.find((n) => {
                 return n.name === category
             })
 
@@ -75,6 +48,7 @@ export class CocktailEdit extends Page {
                     this.cocktailCards?.push(new CocktailCard(name,price,description,categoryName,imageUrls[0],".cocktailWrapper"))
                 }
             })
+
 
         })
     }

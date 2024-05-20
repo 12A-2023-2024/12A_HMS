62458c28d22b6651ff675b4d129c2caf4efed33c
abos-gergo <gergo.abos@gmail.com>
2024-05-20 17:19:37 +0200

    Added dynamic filters

--- src/pages/reservation/reservation.ts
+++ src/pages/reservation/reservation.ts
@@ -22,11 +22,15 @@ export class ReservationPage extends Page {
     customElements.define("result-card", resultCard);
     customElements.define("filter-menu", filterMenu);
 
-    const searchResult = document.getElementById("searchResults");
-    searchResult?.appendChild(
-      new filterMenu(["4People", "2People", "1Person"]),
-    );
-
+    const searchResult = document.getElementById("searchResults") as HTMLElement;
+    const parameterList: string[] = [];
+    API.queryParameters().then((parameters: any) => {
+      parameters.forEach((parameter: { name: string; }) => {
+        parameterList.push(parameter.name)
+      });
+      searchResult.appendChild(new filterMenu(parameterList));
+    });
+    
     addCallBacks();
   }
 }
@@ -70,7 +74,7 @@ function addCallBacks() {
     const searchResult = document.getElementById("searchResults") as HTMLElement;
     API.queryRooms(floor, fromPrice, toPrice, 5, parameters, fromDate, toDate).then((rooms) => {
       searchResult.innerHTML = "";
-      rooms.forEach(room => {
+      rooms.forEach((room: any) => {
         searchResult?.appendChild(new resultCard("asd", "asd", room));
       });
     });
